c92b856fa3d466ae761d6ff3aba45b8edec7dd0f
Mikel Monge <mikelmonge@gmail.com>
2024-03-06 14:02:24 +0100

    Update database settings for MySql, create models for database

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+  ID = models.AutoField(primary_key=True,null=False,unique=True)
+  Title = models.CharField(max_length=255)
+  Price = models.DecimalField(max_digits=10,decimal_places=2,null=False)
+  Inventory = models.IntegerField(default=5)
+
+  def __str__(self):
+    return self.Title
+
+class Booking(models.Model):
+  ID = models.AutoField(primary_key=True,null=False,unique=True)
+  Name = models.CharField(max_length=255)
+  No_of_guests = models.IntegerField(default=6)
+  BookingDate = models.DateField()
+
+  def __str__(self):
+    return self.Name
+
